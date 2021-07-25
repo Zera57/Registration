467e75a14bc00d46c0533daf0c3dbe40dea79e04
Avtonomov Svyatoslav <svetik.avtonomov@gmail.com>
2021-07-25 18:27:33 +0300

    Initial commit

---
+++ src/test/java/com/Zera57/Registration/RegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zera57.Registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
