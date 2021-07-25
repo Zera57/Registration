b6a8d7763bda7dc531a9796461d79f9d87c8c50e
Avtonomov Svyatoslav <svetik.avtonomov@gmail.com>
2021-07-25 19:47:20 +0300

    Maded UserRepository and UserService

---
+++ src/main/java/com/Zera57/Application/User/UserService.java
@@ -0,0 +1,23 @@
+package com.Zera57.Application.User;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND =
+            "user with email %s not found";
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(()
+                -> new UsernameNotFoundException(USER_NOT_FOUND));
+    }
+}
