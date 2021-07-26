cf4040629b5376c92cc748aa0b9ab3ed08c1739b
Avtonomov Svyatoslav <svetik.avtonomov@gmail.com>
2021-07-26 17:35:17 +0300

    Updated User

--- src/main/java/com/Zera57/Application/User/User.java
+++ src/main/java/com/Zera57/Application/User/User.java
@@ -16,6 +16,8 @@ import java.util.Collections;
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
+@Entity
+@Table(name="users")
 public class User implements UserDetails {
     @SequenceGenerator(
             name = "user_sequence",
@@ -28,29 +30,25 @@ public class User implements UserDetails {
             generator = "user_sequence"
     )
     private Long id;
-    private String name;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
-    private Boolean locked;
-    private Boolean enabled;
+    private Boolean locked = false;
+    private Boolean enabled = false;
 
-    public User(String name,
-                String username,
+    public User(String firstName,
+                String lastName,
                 String email,
                 String password,
-                UserRole userRole,
-                Boolean locked,
-                Boolean enabled) {
-        this.name = name;
-        this.username = username;
+                UserRole userRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.userRole = userRole;
-        this.locked = locked;
-        this.enabled = enabled;
     }
 
     @Override
@@ -67,7 +65,7 @@ public class User implements UserDetails {
 
     @Override
     public String getUsername() {
-        return username;
+        return email;
     }
 
     @Override
--- src/main/java/com/Zera57/Application/User/UserService.java
+++ src/main/java/com/Zera57/Application/User/UserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,11 +14,30 @@ public class UserService implements UserDetailsService {
     private final static String USER_NOT_FOUND =
             "user with email %s not found";
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
         return userRepository.findByEmail(email).orElseThrow(()
                 -> new UsernameNotFoundException(USER_NOT_FOUND));
+    }
+
+    public String signUpUser(User user) {
+        boolean userExists = userRepository.findByEmail(user.getEmail()).isPresent();
+
+        if (userExists) {
+            throw new IllegalStateException("email already taken");
+        }
+
+        String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+
+        user.setPassword(encodedPassword);
+
+        userRepository.save(user);
+
+       // TODO: Send confirmation token
+
+        return "it works";
     }
 }
