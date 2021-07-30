d7b31227e9e094f66ba55c25304c8f40c25546c6
Avtonomov Svyatoslav <svetik.avtonomov@gmail.com>
2021-07-30 14:27:39 +0300

    Added Cobfirmation token to project and EmailSender

---
+++ src/main/java/com/Zera57/Application/Email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.Zera57.Application.Email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
--- src/main/java/com/Zera57/Application/User/UserService.java
+++ src/main/java/com/Zera57/Application/User/UserService.java
@@ -1,11 +1,16 @@
 package com.Zera57.Application.User;
 
+import com.Zera57.Application.Registration.token.ConfirmationToken;
+import com.Zera57.Application.Registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -15,6 +20,7 @@ public class UserService implements UserDetailsService {
             "user with email %s not found";
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email)
@@ -36,8 +42,21 @@ public class UserService implements UserDetailsService {
 
         userRepository.save(user);
 
-       // TODO: Send confirmation token
+        String token = UUID.randomUUID().toString();
 
-        return "it works";
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                user
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+        return token;
+    }
+
+    public int enableUser(String email) {
+        return userRepository.enableUser(email);
     }
 }
